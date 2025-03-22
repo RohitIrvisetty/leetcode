e0805406508f7cf218514e9ac93c206cc245afe8
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2025-03-22 23:27:00 +0530

    Time: 92 ms (5%), Space: 47.5 MB (100%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -0,0 +1,16 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+    let index = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+    while (index < nums.length) {
+        nums[index] = 0;
+        index++;
+    }
+};
