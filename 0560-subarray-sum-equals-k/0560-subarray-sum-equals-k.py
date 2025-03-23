b8729728f642ec9bb41ad91a24672a919ddd44ed
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2025-03-23 21:52:03 +0530

    Time: 27 ms (84.72%), Space: 20.4 MB (36.73%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,12 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix_sums = {0: 1}  # Dictionary to store prefix sum frequencies
+        sumMap = {0: 1}
         count = 0
+        n = len(nums)
         current_sum = 0
 
-        for num in nums:
-            current_sum += num  # Update prefix sum
-            count += prefix_sums.get(current_sum - k, 0)  # Check if (current_sum - k) exists
-            prefix_sums[current_sum] = prefix_sums.get(current_sum, 0) + 1  # Store/update prefix sum frequency
-        
+        for i in range(n):
+            current_sum += nums[i]            
+            rem = current_sum - k
+
+            if rem in sumMap:
+                count += sumMap[rem]
+
+            if current_sum not in sumMap:
+                sumMap[current_sum] = 1
+            else:
+                sumMap[current_sum] = sumMap[current_sum] + 1
         return count
