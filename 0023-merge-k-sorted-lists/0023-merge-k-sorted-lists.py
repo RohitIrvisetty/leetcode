d058f68cfa09536f286a1b2beeddb4a2c17b9aa3
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2025-03-29 00:24:59 +0530

    Time: 8 ms (79.34%), Space: 20.4 MB (35.76%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,29 +4,21 @@
 #         self.val = val
 #         self.next = next
 
-class HeapNode:
-    def __init__(self, node):
-        self.node = node
-
-    def __lt__(self, other):
-        # Define comparison based on ListNode's value
-        return self.node.val < other.node.val
-
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         min_heap = []
-        for i in lists:
-            if i:
-                heapq.heappush(min_heap, HeapNode(i))
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(min_heap, (node.val, i, node))
         
         dummy = ListNode(-1)
         current = dummy
         while min_heap:
-            heapnode = heapq.heappop(min_heap)
-            node = heapnode.node
-            current.next = node
+            min_val, idx, heapnode = heapq.heappop(min_heap)
+            
+            current.next = heapnode
             current = current.next
-            if node.next:
-                heapq.heappush(min_heap, HeapNode(node.next))         
+            if heapnode.next:
+                heapq.heappush(min_heap, (heapnode.next.val, idx, heapnode.next))         
         return dummy.next
         
