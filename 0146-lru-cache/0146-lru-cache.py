18972e259d903f2eb8ba7ede4f79defc02be7bd9
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2025-03-29 08:37:52 +0530

    Time: 132 ms (67.44%), Space: 78.6 MB (27.45%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,26 +1,52 @@
+class Node:
+    def __init__(self, key, next = None, prev = None):
+        self.key = key
+        self.next = next
+        self.prev = prev
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity=capacity
-        self.dic=collections.OrderedDict()
-        
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-        if key not in self.dic:
+        if key not in self.cache:
             return -1
-        
-        self.dic.move_to_end(key)
-        
-        return self.dic[key]
-    def put(self, key: int, value: int) -> None:
-        if key in self.dic:
-            self.dic.move_to_end(key)
-        
-        self.dic[key] = value
-        if len(self.dic) > self.capacity:
-            self.dic.popitem(False)
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        node, value = self.cache[key]
+        self._remove(node)
+        self._add_to_front(node)
+        return value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node, value1 = self.cache[key]
+            self._remove(node)
+            self._add_to_front(node)
+            self.cache[key] = (node, value)
+        else:
+            if len(self.cache) >= self.capacity:
+                lru = self.tail.prev
+                self._remove(lru)
+                del self.cache[lru.key] 
+            node = Node(key)
+            self._add_to_front(node)
+            self.cache[key] = (node, value)
+            
+    
+    def _add_to_front(self, node):
+        node.prev = self.head
+        headNext = self.head.next
+        self.head.next = node
+        node.next = headNext
+        headNext.prev = node
+
+    def _remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
