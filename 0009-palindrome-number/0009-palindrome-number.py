4f09bb51d0fa61079eb21af54cf97eca685a30cd
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2025-06-03 13:26:42 +0530

    Time: 7 ms (62.22%), Space: 17.7 MB (63.79%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,21 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        sign = 1
-        if x < 0:
-            sign = -1
-        revNum = int(str(sign * x)[::-1])
-        return x == revNum
+    def isPalindrome(self, n: int) -> bool:
+        copy = n
+        
+        # After the code, revNum will
+        # contain the reversed number
+        revNum = 0
+
+        # Keep on iterating while there
+        # are digits left to extract
+        while n > 0:
+            lastDigit = n % 10
+
+            # Pushing last digit at the
+            # back of reversed number
+            revNum = (revNum * 10) + lastDigit
+            n = n // 10
+        
+        # Return true if the reversed and 
+        # copy of original number is same
+        return revNum == copy
