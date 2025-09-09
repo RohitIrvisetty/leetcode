91c782a68a5f7bd8c8c316c0216d2c50e44d3702
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2025-09-08 19:42:48 -0700

    Time: 7 ms (1.15%), Space: 18.1 MB (14.42%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -1,25 +1,34 @@
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        if left == right:
+        if not head or left == right:
             return head
-        dummy = ListNode(-1)
-        prev = dummy
-        prev.next = head
 
-        for i in range(left - 1):
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        # Move prev to the node just before 'left'
+        for _ in range(left - 1):
             prev = prev.next
 
+
+        # Reverse the sublist from left to right
         curr = prev.next
         prev_sublist = None
 
-        for i in range(right - left + 1):
-            nextNode = curr.next
+        for _ in range(right - left + 1):
+            next_node = curr.next
             curr.next = prev_sublist
             prev_sublist = curr
-            curr = nextNode
-        
+            curr = next_node
         print(prev_sublist)
-
+        print(curr)
+        print(prev)
+        # Connect the reversed sublist back to the original list
+        
         prev.next.next = curr
+        print(prev)
         prev.next = prev_sublist
+        print(prev)
+        
         return dummy.next
