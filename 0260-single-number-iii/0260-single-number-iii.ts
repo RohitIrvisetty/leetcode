0dfb322a8a7a8e06c76b3d077a078e687ce928e3
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2025-11-20 19:23:10 -0800

    Time: 552 ms (6.15%), Space: 43.2 MB (100%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.ts
+++ 0260-single-number-iii/0260-single-number-iii.ts
@@ -1,16 +1,3 @@
 function singleNumber(nums: number[]): number[] {
-    let xorVallue: number = 0;
-    for (let i = 0; i < nums.length; i++) {
-        xorVallue ^= nums[i];
-    }
-    let a, b = 0;
-    let d = xorVallue & -xorVallue;
-    for (let i = 0; i < nums.length; i++) {
-        if (d & nums[i]) {
-            a ^= nums[i];
-        } else {
-            b ^= nums[i];
-        }
-    }
-    return [a, b];
+    return nums.filter((x) => nums.indexOf(x) == nums.lastIndexOf(x)); 
 };
