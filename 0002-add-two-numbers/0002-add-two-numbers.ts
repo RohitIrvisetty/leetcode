1d105fb08dc65be72fb69046ce673a1680f239a0
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2025-11-29 20:22:46 -0800

    Time: 85 ms (5.01%), Space: 48.4 MB (100%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.ts
+++ 0002-add-two-numbers/0002-add-two-numbers.ts
@@ -11,23 +11,17 @@
  */
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    let firstArr: ListNode | null;
-    let secondArr: ListNode | null;
-    let output: ListNode = new ListNode(0);
-    let now = output;
-    //console.log(output);
-    let carry: number = 0;
-    console.log("1", l1.next);
-    while (l1 != null || l2 != null) {
-        let sum: number = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
-        now.next = new ListNode(sum % 10);
-        now = now.next;
+    let output = new ListNode(0);
+    let currentNode = output;
+    let carry = 0;
+    while (l1 || l2) {
+        let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
         carry = sum > 9 ? 1 : 0;
+        currentNode.next = new ListNode(sum%10);
+        currentNode = currentNode.next;
         l1 = l1 ? l1.next : null;
         l2 = l2 ? l2.next : null;
     }
-    if (carry) now.next = new ListNode(carry);
-    console.log(output);
-    console.log(now);
+    if (carry) currentNode.next = new ListNode(carry)
     return output.next;
 };
