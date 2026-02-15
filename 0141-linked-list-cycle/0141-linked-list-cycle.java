442aed69ef9af9366498bedefa5083dc5d404745
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-02-15 08:30:37 -0800

    Time: 0 ms (100%), Space: 47 MB (31.41%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,11 +11,11 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+
         ListNode slow = head;
         ListNode fast = head;
-        
 
-        while (fast != null && fast.next != null) {
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
 
@@ -23,6 +23,7 @@ public class Solution {
                 return true;
             }
         }
+
         return false;
     }
 }
