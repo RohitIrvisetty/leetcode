43adc947bfb53deeed2dec571e51c787205aa94b
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-02-17 12:11:45 -0800

    Time: 1 ms (99.73%), Space: 48.2 MB (39.2%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,21 +1,17 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int xorResult = 0;
-        
+        int xorResult = 0, a = 0, b = 0;
+
         for (int i = 0; i < nums.length; i++) {
             xorResult ^= nums[i];
         }
 
-        int diff = xorResult & -xorResult;
-
-        int a = 0;
-        int b = 0;
-
-        for (int num: nums) {
-            if ((num & diff) != 0) {
-                a ^= num;
+        int diff = xorResult & (-xorResult);
+        for (int i = 0; i < nums.length; i++) {
+            if ((diff & nums[i]) == 0) {
+                a ^= nums[i];
             } else {
-                b ^= num;
+                b ^= nums[i];
             }
         }
         return new int[] {a, b};
