6f69a92a95a8434bf8defaec1a64db8833dd216b
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-02-19 14:27:21 -0800

    Time: 1 ms (93.7%), Space: 45.5 MB (51.23%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,21 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
+        int ones = 0, twos = 0;
 
-        for (int i = 0; i < 32; i++) {
-            int sum = 0;
-
-            for (int num: nums) {
-                if(((num >> i) & 1) == 1) {
-                    sum += 1;
-                }
-             }
-             
-             if(sum % 3 != 0) {
-                result = result | (1 << i);
-             }
-
+        for (int num: nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
         }
-        return result;
+
+        return ones;
     }
 }
