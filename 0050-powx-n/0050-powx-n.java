d1271af5d68f61caab1cff08099d40630a74f231
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-02-21 14:53:00 -0800

    Time: 0 ms (100%), Space: 47.7 MB (79.15%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -9,13 +9,12 @@ class Solution {
 
     private double power(double x, long n) {
         if (n == 0) return 1;
-
-        double half = power(x, n / 2);
+        if (n == 1) return x;
 
         if (n % 2 == 0) {
-            return half * half;
+            return power(x * x, n / 2);
         } else {
-            return half * half * x;
+            return x * power(x, n - 1);
         }
     }
 }
