8730e1e1e0d4dc3d747ea9a0f599a2fa6b905274
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-02-21 12:39:42 -0800

    Time: 9 ms (42.8%), Space: 42.3 MB (14.19%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,15 +2,6 @@ class Solution {
     public int fib(int n) {
         if (n <= 1) return n;
 
-        int prev1 = 0, prev2 = 1;
-
-        for (int i = 2; i <= n; i++) {
-            int sum = prev1 + prev2;
-            
-            prev1 = prev2;
-            prev2 = sum;
-        }
-
-        return prev2;
+        return fib(n - 1) + fib(n - 2);
     }
 }
