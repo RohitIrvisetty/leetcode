28d27af6899a135bcd17fe4c3e481f27ab70cebc
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-02-26 13:15:52 -0800

    Time: 7 ms (10.09%), Space: 44.1 MB (7.84%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.java
+++ 0468-validate-ip-address/0468-validate-ip-address.java
@@ -1,49 +1,14 @@
 class Solution {
     public String validIPAddress(String queryIP) {
-        if (isIPv4(queryIP)) return "IPv4";
-        if (isIPv6(queryIP)) return "IPv6";
+        String ipv4 = 
+        "(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}" +
+        "([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])";
+
+        String ipv6 = 
+        "([0-9a-fA-F]{1,4}:){7}" + "[0-9a-fA-F]{1,4}";
+
+        if (queryIP.matches(ipv4)) return "IPv4";
+        if (queryIP.matches(ipv6)) return "IPv6";
         return "Neither";
-    }
-
-    private boolean isIPv4(String ip) {
-        String[] parts = ip.split("\\.", -1);
-        if (parts.length != 4) return false;
-
-        for (String part : parts) {
-            if (part.length() == 0 || part.length() > 3) return false;
-
-            // Leading zero check
-            if (part.length() > 1 && part.charAt(0) == '0') return false;
-
-            for (char c : part.toCharArray()) {
-                if (!Character.isDigit(c)) return false;
-            }
-
-            int num = Integer.parseInt(part);
-            if (num < 0 || num > 255) return false;
-        }
-
-        return true;
-    }
-
-    private boolean isIPv6(String ip) {
-        String[] parts = ip.split(":", -1);
-        if (parts.length != 8) return false;
-
-        for (String part : parts) {
-            if (part.length() == 0 || part.length() > 4) return false;
-
-            for (char c : part.toCharArray()) {
-                if (!isHexChar(c)) return false;
-            }
-        }
-
-        return true;
-    }
-
-    private boolean isHexChar(char c) {
-        return (c >= '0' && c <= '9') ||
-               (c >= 'a' && c <= 'f') ||
-               (c >= 'A' && c <= 'F');
     }
 }
