21bb5701542ee45e9ac17feb7007481cbf5bb095
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-03-22 00:57:44 -0700

    Time: 3 ms (37.5%), Space: 43.1 MB (31.07%) - LeetHub

--- 0483-smallest-good-base/0483-smallest-good-base.java
+++ 0483-smallest-good-base/0483-smallest-good-base.java
@@ -2,26 +2,31 @@ class Solution {
     public String smallestGoodBase(String n) {
         long num = Long.parseLong(n);
 
-        int maxM = (int)(Math.log(num) / Math.log(2));
+        int maxM = (int) (Math.log(num) / Math.log(2));
 
         for (int m = maxM; m > 1; m--) {
-            long k = (long)Math.pow(num, 1.0 / m);
+            long k = (long) Math.pow(num, 1.0 / m);
 
-            if (k <= 1) continue;
+            for (long candidate = k; candidate <= k + 1; candidate++) {
+                long sum = 1, curr = 1;
 
-            long sum = 1;
-            long curr = 1;
+                for (int i = 0; i < m; i++) {
+                    curr *= candidate;
+                    if (curr > num) {
+                        break;
+                    }
 
-            for (int i = 1; i <= m; i++) {
-                curr *= k;
-                sum += curr;
-            }
+                    sum += curr;
+                    if (sum > num) {
+                        break;
+                    }
+                }
 
-            if (sum == num) {
-                return String.valueOf(k);
+                if (sum == num) {
+                    return String.valueOf(candidate);
+                }
             }
         }
-
         return String.valueOf(num - 1);
     }
 }
