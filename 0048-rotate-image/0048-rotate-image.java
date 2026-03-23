564236590f5d7145343c8579c317edadd3eef427
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-03-23 14:59:49 -0700

    Time: 0 ms (100%), Space: 44.1 MB (11.17%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,34 +1,34 @@
 class Solution {
-    private void swap(int[][] matrix, int i, int j) {
-        if (i == j) return;
-        matrix[i][j] = matrix[i][j] ^ matrix[j][i];
-        matrix[j][i] = matrix[i][j] ^ matrix[j][i];
-        matrix[i][j] = matrix[i][j] ^ matrix[j][i];
-    }
-
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        // Step 1: Transpose
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                swap(matrix, i, j);
+        // process layer by layer
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+
+            for (int i = first; i < last; i++) {
+                int top = matrix[first][i];
+
+                int right = matrix[i][last];
+
+                int bottom = matrix[last][n - 1 - i];
+
+                int left = matrix[n - 1 - i][first];
+
+                // left -> top
+                matrix[first][i] = matrix[n - 1 - i][first];
+
+                // bottom -> left
+                matrix[n - 1 - i][first] = matrix[last][n - 1 - i];
+
+                // right -> bottom
+                matrix[last][n - 1 - i] = matrix[i][last];
+
+                // top -> right
+                matrix[i][last] = top;
             }
-        }
 
-        // Step 2: Reverse each row
-        for (int i = 0; i < n; i++) {
-            int left = 0;
-            int right = n - 1;
-
-            while (left < right) {
-                int temp = matrix[i][left];
-                matrix[i][left] = matrix[i][right];
-                matrix[i][right] = temp;
-
-                left++;
-                right--;
-            }
         }
     }
 }
