6e5c9afd7f38e642fbaa772dabd399acd46c05fd
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-03-27 09:47:15 -0700

    Time: 7 ms (48.99%), Space: 67 MB (89.32%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,23 +1,31 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        int n = nums.length;
+        
+        // cyclic sort
+
+        int i = 0, n = nums.length;
         List<Integer> missing = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            int index = Math.abs(nums[i]) - 1;
-
-            if (nums[index] > 0) {
-                nums[index] = -nums[index];
+        while (i < n) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] != nums[correctIndex]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                i++;
             }
         }
 
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                missing.add(i + 1);
+        for (int j = 0; j < n; j++) {
+            if (nums[j] != j + 1) {
+                missing.add(j + 1);
             }
         }
-
         return missing;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        if (i == j) return;
+        nums[i] = nums[i] ^ nums[j];
+        nums[j] = nums[i] ^ nums[j];
+        nums[i] = nums[i] ^ nums[j]; 
     }
 }
