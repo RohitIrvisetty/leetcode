42f8e4546f5ed60ea760b7cdfc1cbfe86d740db1
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-04-27 16:19:03 -0700

    Time: 0 ms (100%), Space: 42.8 MB (36.57%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        
+        if (pattern.length() != words.length) return false;
+        
+        Map<Object, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+            
+            if (!Objects.equals(map.put(c, i), map.put(word, i))) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
