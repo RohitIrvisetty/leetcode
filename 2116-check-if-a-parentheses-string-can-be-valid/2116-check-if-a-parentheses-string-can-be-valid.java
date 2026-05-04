6bffb8ed0b69d90ba64f823affc237cd344a4529
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-05-03 21:41:53 -0700

    Time: 9 ms (100%), Space: 47.6 MB (91.01%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
@@ -1,42 +1,32 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
         int n = s.length();
-        StringBuilder updatedString = new StringBuilder();
-
         if (n % 2 != 0) return false;
 
-        for (int i = 0; i < n; i++) {
-            if (locked.charAt(i) == '0') {
-                updatedString.append('*');
-            } else {
-                updatedString.append(s.charAt(i));
-            }
-        }
-
-        System.out.println(updatedString.toString());
-        Stack<Integer> starStack = new Stack<>();
-        Stack<Integer> openStack = new Stack<>();
+        int minOpen = 0, maxOpen = 0;
 
         for (int i = 0; i < n; i++) {
-            if (updatedString.charAt(i) == '(') {
-                openStack.push(i);
-            } else if (updatedString.charAt(i) == '*') {
-                starStack.push(i);
-            } else {
-                if (openStack.size() > 0) {
-                    openStack.pop();
-                } else if (starStack.size() > 0) {
-                    starStack.pop();
+            char ch = s.charAt(i);
+
+            if (locked.charAt(i) == '1') {
+                if (ch == '(') {
+                    minOpen++;
+                    maxOpen++;
                 } else {
-                    return false;
+                    minOpen--;
+                    maxOpen--;
                 }
+            } else {
+                // flexible
+                minOpen--;   // treat as ')'
+                maxOpen++;   // treat as '('
             }
+
+            if (maxOpen < 0) return false;
+
+            minOpen = Math.max(minOpen, 0);
         }
 
-        while (!openStack.isEmpty() && !starStack.isEmpty() && openStack.peek() < starStack.peek()) {
-            openStack.pop();
-            starStack.pop();
-        }
-        return openStack.isEmpty() && (starStack.size() % 2 == 0);
+        return minOpen == 0;
     }
 }
