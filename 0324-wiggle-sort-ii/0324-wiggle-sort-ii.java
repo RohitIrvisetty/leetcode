52fa6bfb5783dca61e4a581af6b2ac6b80163625
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-05-11 05:09:34 -0700

    Time: 253 ms (5.6%), Space: 51.9 MB (5.6%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -1,22 +1,75 @@
+import java.util.*;
+
 class Solution {
+
     public void wiggleSort(int[] nums) {
-        Arrays.sort(nums);
-
         int n = nums.length;
-        int[] res = new int[n];
 
-        int left = (n - 1) / 2;  // end of small half
-        int right = n - 1;       // end of large half
+        int median = findKthLargest(nums, (n) / 2);
 
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                res[i] = nums[left--];
+        int left = 0, mid = 0, right = n - 1;
+        
+        System.out.println(median + " " + Arrays.toString(nums));
+        
+        while (mid <= right) {
+            System.out.println(nums[newIndex(mid, n)]);
+            if (nums[newIndex(mid, n)] > median) {
+                //System.out.println(nums[newIndex(mid, n)]);
+                swap(nums,
+                     newIndex(left, n),
+                     newIndex(mid, n));
+                mid++;
+                left++;
+            } else if (nums[newIndex(mid, n)] == median) {
+                mid++;                
             } else {
-                res[i] = nums[right--];
+                swap(nums,
+                     newIndex(right, n),
+                     newIndex(mid, n));
+                right--;
+            }
+        }
+    }
+
+    private int newIndex(int index, int n) {
+        return (1 + 2 * index) % (n | 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // QuickSelect for kth largest
+    private int findKthLargest(int[] nums, int k) {
+        int target = nums.length - k - 1;
+
+        int left = 0, right = nums.length - 1;
+
+        while (true) {
+            int pivot = partition(nums, left, right);
+
+            if (pivot == target)
+                return nums[pivot];
+            else if (pivot < target)
+                left = pivot + 1;
+            else
+                right = pivot - 1;
+        }
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int p = left;
+
+        for (int i = left; i < right; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums, i, p++);
             }
         }
 
-        // copy back
-        System.arraycopy(res, 0, nums, 0, n);
+        swap(nums, p, right);
+        return p;
     }
 }
