891999b4cb87af735f4fe936a01e99c8e9e78bce
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-05-22 16:59:45 -0700

    Time: 40 ms (5.74%), Space: 83 MB (44.77%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,22 +1,32 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[0];
+        int i = 0, n = nums.length;
 
-        int n = nums.length;
+        while (i < n) {
+            int correctIndex = nums[i] - 1;
 
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        slow = nums[0];
-
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+            if (nums[i] <= n && nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
         }
 
-        return slow;
+        System.out.println(Arrays.toString(nums));
+        for (int j = 1; j <= n; j++) {
+            if (j != nums[j - 1]) {
+                return nums[j - 1];
+            }
+        }
+
+        return n;
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        if (left == right) return;
+
+        nums[left] = nums[left] ^ nums[right];
+        nums[right] = nums[left] ^ nums[right];
+        nums[left] = nums[left] ^ nums[right];
     }
 }
