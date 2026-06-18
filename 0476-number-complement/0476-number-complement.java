8938fcc02b5d42fb5512c272d15ed64d8f11f0da
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-06-18 01:14:05 -0700

    Time: 0 ms (100%), Space: 42.2 MB (37.81%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findComplement(int num) {
+        int bits = 0;
+        int temp = num;
+        while(temp != 0)
+        {
+            bits++;
+            temp = temp >> 1;
+        }
+        int mask = (1 << bits) - 1;
+        return ~num & mask;
+    }
+}
