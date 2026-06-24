a8f607c47431cace31bf9eb6fb2224ddbe6ccac3
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-06-24 14:43:25 -0700

    Time: 13 ms (90.98%), Space: 59.5 MB (37.76%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -3,16 +3,16 @@ class Solution {
         int balance = 0;
         int maxImbalance = 0;
 
-        for (char ch: s.toCharArray()) {
+        for (char ch : s.toCharArray()) {
             if (ch == '[') {
                 balance++;
             } else {
                 balance--;
+                if (balance < 0) {
+                    maxImbalance = Math.min(maxImbalance, balance);
+                }
             }
 
-            if (balance < 0) {
-                maxImbalance = Math.min(maxImbalance, balance);
-            }
         }
 
         return (-1 * maxImbalance + 1) / 2;
