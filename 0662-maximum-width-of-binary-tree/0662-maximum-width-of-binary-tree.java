2f0f26c08abbe7a524b0fa1f7d56b8a79193fffd
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-06-24 11:54:46 -0700

    Time: 2 ms (40.46%), Space: 45.3 MB (31.79%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -14,30 +14,32 @@
  * }
  */
 class Solution {
+    private record pair(TreeNode root, int val) {}
+
     public int widthOfBinaryTree(TreeNode root) {
         
-        LinkedList<Pair<TreeNode, Integer>> list = new LinkedList<>();
-        list.offer(new Pair<>(root, 1));
+        LinkedList<pair> list = new LinkedList<>();
+        list.offer(new pair(root, 1));
       
         int maxWidth = 0;
       
         while (!list.isEmpty()) {
             
-            int currentLevelWidth = list.peekLast().getValue() - list.peekFirst().getValue() + 1;
+            int currentLevelWidth = list.peekLast().val() - list.peekFirst().val() + 1;
             maxWidth = Math.max(maxWidth, currentLevelWidth);
           
             int levelSize = list.size();
             for (int i = 0; i < levelSize; i++) {
-                Pair<TreeNode, Integer> currentPair = list.poll();
-                TreeNode currentNode = currentPair.getKey();
-                int currentPosition = currentPair.getValue();
+                pair currentPair = list.poll();
+                TreeNode currentNode = currentPair.root();
+                int currentPosition = currentPair.val();
               
                 if (currentNode.left != null) {
-                    list.offer(new Pair<>(currentNode.left, currentPosition * 2));
+                    list.offer(new pair(currentNode.left, currentPosition * 2));
                 }
               
                 if (currentNode.right != null) {
-                    list.offer(new Pair<>(currentNode.right, currentPosition * 2 + 1));
+                    list.offer(new pair(currentNode.right, currentPosition * 2 + 1));
                 }
             }
         }
