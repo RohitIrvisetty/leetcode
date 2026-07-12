5581fe51036203271d680e7a45c1471802a0ce87
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-07-12 13:42:52 -0700

    Time: 2 ms (71.14%), Space: 45.2 MB (8.89%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        backtrack("", 0, 0, n, ans);
+        String[] parenthesesChoices = new String[]{"(", ")"};
+        backtrack("", 0, 0, n, parenthesesChoices, ans);
         return ans;
     }
 
-    private void backtrack(String s, int open, int close, int n, List<String> ans) {
+    private void backtrack(String s, int open, int close, int n, String[] parenthesesChoices, List<String> ans) {
 
         if (open + close == 2 * n) {
             ans.add(s);
             return;
         }
 
-        if (open < n) {
-            backtrack(s + '(', open + 1, close, n, ans);
-        }
-
-        if (close < open) {
-            backtrack(s + ')', open, close + 1, n, ans);
+        for (int choice = 0; choice < parenthesesChoices.length; choice++) {
+            if (parenthesesChoices[choice] == "(" && open < n) {
+                backtrack(s + parenthesesChoices[choice], open + 1, close, n, parenthesesChoices, ans);
+            } else if (parenthesesChoices[choice] == ")" && close < open){
+                backtrack(s + parenthesesChoices[choice], open, close + 1, n, parenthesesChoices, ans);
+            }
         }
     }
 }
