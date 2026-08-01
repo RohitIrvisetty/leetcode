98da5d8767182180100138a43d01ca961d644ef0
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-08-01 13:23:22 -0700

    Time: 6 ms (15.38%), Space: 43.1 MB (9.62%) - LeetHub

--- 0483-smallest-good-base/0483-smallest-good-base.java
+++ 0483-smallest-good-base/0483-smallest-good-base.java
@@ -1,31 +1,55 @@
 class Solution {
+
     public String smallestGoodBase(String n) {
         long num = Long.parseLong(n);
 
+        // maximum exponent
         int maxM = (int) (Math.log(num) / Math.log(2));
 
-        for (int m = maxM; m > 1; m--) {
-            long k = (long) Math.pow(num, 1.0 / m);
+        for (int m = maxM; m >= 1; m--) {
 
-                long sum = 1, curr = 1;
+            long left = 2;
+            long right = (long) Math.pow(num, 1.0 / m);
+
+            while (left <= right) {
+
+                long mid = left + (right - left) / 2;
+
+                long sum = 1;
+                long curr = 1;
+
+                boolean overflow = false;
 
                 for (int i = 0; i < m; i++) {
-                    curr *= k;
-                    if (curr > num) {
+
+                    if (curr > num / mid) {
+                        overflow = true;
+                        break;
+                    }
+
+                    curr *= mid;
+
+                    if (sum > num - curr) {
+                        overflow = true;
                         break;
                     }
 
                     sum += curr;
-                    if (sum > num) {
-                        break;
-                    }
                 }
 
-                if (sum == num) {
-                    return String.valueOf(k);
+                if (!overflow && sum == num) {
+                    return String.valueOf(mid);
                 }
-            
+
+                if (overflow || sum > num) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
         }
+
+        // always valid: n = 11 in base (n-1)
         return String.valueOf(num - 1);
     }
 }
