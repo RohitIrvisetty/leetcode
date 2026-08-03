c9f476a7793c0d904f8d9ec1f0488e6019b0c0f4
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-08-03 09:45:15 -0700

    Time: 0 ms (100%), Space: 44.4 MB (18.97%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -8,12 +8,12 @@ class Solution {
 
             if (nums[mid] < nums[mid + 1]) {
                 left = mid + 1;
+            } else if (nums[mid] == nums[mid + 1]) {
+                right = mid - 1;
             } else {
                 right = mid;
             }
-
         }
-
         return left;
     }
 }
