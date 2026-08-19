b297f5439c459922c5fb070b68258163e253a67d
Rohit Irvisetty <62420528+RohitIrvisetty@users.noreply.github.com>
2026-08-19 09:49:13 -0700

    Time: 1 ms (97.99%), Space: 42.3 MB (76.38%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.java
+++ 3133-minimum-array-end/3133-minimum-array-end.java
@@ -1,10 +1,21 @@
 class Solution {
     public long minEnd(int n, int x) {
         long ans = x;
+        long k = n - 1L;
 
-        for (int i = 1; i < n; i++) {
-            ans = (ans + 1) | x;
-        } 
+        for (int bit = 0; k > 0; bit++) {
+            // This bit position is available
+            if ((x & (1L << bit)) == 0) {
+
+                // Put the lowest bit of k here
+                if ((k & 1L) != 0) {
+                    ans |= (1L << bit);
+                }
+
+                // Move to the next bit of k
+                k >>= 1;
+            }
+        }
 
         return ans;
     }
